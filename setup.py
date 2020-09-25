1395061f5eb7bf5f2121df2817da2b465f09c9b9
Alex Z <alex.zhang1012@gmail.com>
2020-09-24 18:33:42 -0700

    generate setup.py init.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+# coding:utf-8
+
+from setuptools import setup
+# or
+# from distutils.core import setup
+
+setup(
+    name='pypcie',
+    version='0.1',
+    description='Pcie utils',
+    author='Alex',
+    author_email='alex.zhang1012@gmail.com',
+    url='https://github.com/shmily1012/pypcie',
+    packages=['pcie'],
+)
